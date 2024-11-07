a1bb784b5cf5500f947525c314f321d604998add
JonanLowe <jonathan_a_lowe@hotmail.com>
2024-11-07 14:07:36 +0000

    add contextual delete buttons to comments

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,9 +1,21 @@
+import { useContext } from "react";
+import UserContext from "../contexts/userContext.js"
+
 export default function CommentCard(props){
     const {author, body} = props;
+    const { user } = useContext(UserContext);
+
     return (
-        <section className="comment-card">
+        <>
+        <section className= "comment-card">
+            <div>
             <p><b>Author: {author} </b></p>
             <p>{body}</p>
+            </div>
+            { author === user ? <button> delete comment</button>
+             : null
+            }
         </section>
+        </>
     )
 }
